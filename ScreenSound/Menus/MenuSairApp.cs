efbf9b6a94345df3c1155498f92c0411b640042c
Mateus_Barros <matteusmaciel40@gmail.com>
2023-10-14 20:57:17 -0300

    💻 CSHarp Program

---
+++ ScreenSound/Menus/MenuSairApp.cs
@@ -0,0 +1,12 @@
+﻿
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuSairApp : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Environment.Exit(0);
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,68 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+
+Banda banda01 = new("Banda01");
+banda01.AdicionarNota(new Avaliacao(10));
+banda01.AdicionarNota(new Avaliacao(8));
+banda01.AdicionarNota(new Avaliacao(9));
+
+Banda banda02 = new("Banda02");
+
+Dictionary<string, Banda> bandasRegistradas = new();
+bandasRegistradas.Add(banda01.Nome, banda01);
+bandasRegistradas.Add(banda02.Nome, banda02);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarBanda());
+opcoes.Add(2, new MenuRegistrarAlbum());
+opcoes.Add(3, new MenuBandasRegistradas());
+opcoes.Add(4, new MenuAvaliarBanda());
+opcoes.Add(5, new MenuAvaliarAlbum());
+opcoes.Add(6, new MenuExibirDetalhes());
+opcoes.Add(-1, new MenuSairApp());
+
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+
+    Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+    Console.WriteLine("Digite 3 para mostrar todas as bandas");
+    Console.WriteLine("Digite 4 para avaliar uma Banda");
+    Console.WriteLine("Digite 5 para avaliar uma Album");
+    Console.WriteLine("Digite 6 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+    {
+        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(bandasRegistradas);
+
+        if (opcaoEscolhidaNumerica > 0)
+            ExibirOpcoesDoMenu();
+    }
+    else
+        Console.WriteLine("Opção Invalida...");
+}
+
+ExibirOpcoesDoMenu();
